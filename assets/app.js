bf34de071258bde7325abb1d22e286e6d4e58f62
UlanNSA <erikov25.05@gmail.com>
2021-01-15 22:29:26 +0600

    Finished my part. Redistributed the path to images. Now they're all in a folder asses

--- assets/app.js
+++ assets/app.js
@@ -1,74 +1,30 @@
-///variables
-//window.addEventListener('scroll', function() { document.getElementById('showScroll').innerHTML = pageYOffset + 'px';});
-//alert(1)
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+$(document).ready(function () {
+  var maxSlides = $(".item").length;
+  var current = 1;
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}	
-
-(function(){
-  var maxHeight = 0;
-  $('.card').each(function(){
-    if ($(this).height() > maxHeight) {
-      maxHeight = $(this).height();
+  $(".btn-prev").click(function () {
+    if (current === 1) {
+      current = maxSlides;
+      showSlide();
+    } else {
+      current--;
+      showSlide();
     }
   });
-  $('.card').each(function(){
-    $(this).css('min-height', maxHeight);
+
+  $(".btn-next").click(function () {
+    if (current === maxSlides) {
+      current = 1;
+      showSlide();
+    } else {
+      current++;
+      showSlide();
+    }
   });
-})();
 
+  function showSlide() {
+    $(".item").removeClass("active");
 
-
-
-var timer;
-
-var compareDate = new Date();
-compareDate.setDate(compareDate.getDate() + 4); //just for this demo today + 7 days
-
-timer = setInterval(function () {
-  timeBetweenDates(compareDate);
-}, 1000);
-
-function timeBetweenDates(toDate) {
-  var dateEntered = toDate;
-  var now = new Date();
-  var difference = dateEntered.getTime() - now.getTime();
-
-  if (difference <= 0) {
-    // Timer done
-    clearInterval(timer);
-  } else {
-    var seconds = Math.floor(difference / 1000);
-    var minutes = Math.floor(seconds / 60);
-    var hours = Math.floor(minutes / 60);
-    var days = Math.floor(hours / 24);
-
-    hours %= 24;
-    minutes %= 60;
-    seconds %= 60;
-
-    $("#days").text(days);
-    $("#hours").text(hours);
-    $("#minutes").text(minutes);
-    $("#seconds").text(seconds);
+    $('.item[data-id="' + current + '"]').addClass("active");
   }
-}
-
-
-
-
-// BLOG
-/* Demo purposes only */
-$(".hover").mouseleave(
-  function () {
-    $(this).removeClass("hover");
-  }
-);
-
-
+});
